d2183589ed39788e81c6868167185b5064dbfbf9
BRUGUSOFT <kiran.paturi@brugu.io>
2023-10-16 21:27:07 +0530

    added bootstrap to invesco app

---
+++ invescoapp/src/App.js
@@ -0,0 +1,12 @@
+//Every component should start with capital letter
+const App = ()=>{
+  //every component must have a return statement
+  //it should return only only root tag
+  return(
+    <div>
+      <h1>Welcome to Invesco</h1>
+    </div>
+  )
+}
+//every component must be exported.
+export default App;
---
+++ invescoapp/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import App from './App';
+
+// find the element with the id 'root' in the index.html
+// Wrap the entire app inside that Element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
+
