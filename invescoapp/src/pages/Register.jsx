b6c017cbb47cd664ad91f2b78b5bbaa51b1df52b
BRUGUSOFT <kiran.paturi@brugu.io>
2023-10-17 19:58:14 +0530

    added fetch

--- invescoapp/src/pages/Register.jsx
+++ invescoapp/src/pages/Register.jsx
@@ -1,27 +1,37 @@
 
+import { useEffect } from "react";
 import Dropdown from "../components/Dropdown";
 import Textbox from "../components/Textbox";
 import { RegisterConfig } from "../config/register";
 
 const Register = () => {
     const countryList = [{
-        name:"India",
-        value:"IN"
+        name: "India",
+        value: "IN"
     },
-{
-    name:"United Kingdom",
-    value:"UK"
-},
-{
-    name:"United States of America",
-    value:"USA"
-},]
+    {
+        name: "United Kingdom",
+        value: "UK"
+    },
+    {
+        name: "United States of America",
+        value: "USA"
+    }];
+    useEffect(()=>{
+        fetch("https://restcountries.com/v2/all").then(response=>{
+            return response.json()
+        }).then(result=>{
+            console.log(result);
+        }).catch(ex=>{
+            console.log(ex);
+        })
+    },[]);
     return (
         <div className="container mt-5">
-            <Textbox config ={RegisterConfig.Username}/>
-            <Textbox config ={RegisterConfig.Password}/>
-            <Textbox config ={RegisterConfig.ConfirmPassword}/>
-            <Dropdown config={RegisterConfig.Country} list={countryList}/>
+            <Textbox config={RegisterConfig.Username} />
+            <Textbox config={RegisterConfig.Password} />
+            <Textbox config={RegisterConfig.ConfirmPassword} />
+            <Dropdown config={RegisterConfig.Country} list={countryList} />
             <div class="mb-3">
                 <button className="btn btn-primary">Register</button>
             </div>
