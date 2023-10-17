4a0e191ae3176fdbdcac7f290745e1712b3339e8
BRUGUSOFT <kiran.paturi@brugu.io>
2023-10-17 18:06:17 +0530

    Added Nav Bar

--- invescoapp/src/App.js
+++ invescoapp/src/App.js
@@ -1,10 +1,12 @@
+
+import Header from "./components/Header";
 //Every component should start with capital letter
 const App = ()=>{
   //every component must have a return statement
   //it should return only only root tag
   return(
     <div>
-      <h1>Welcome to Invesco</h1>
+      <Header/>
     </div>
   )
 }
